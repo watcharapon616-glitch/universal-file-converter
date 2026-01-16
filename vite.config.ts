58e22ed520890c0741a52d36e8505d5f3a288eaa
watcharapon616 <watcharapon616@gmail.com>
2026-01-16 16:42:05 +0700

    Fix 404 by updating base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // ใช้ './' เพื่อให้รองรับทั้งโดเมนส่วนตัวและลิงก์ GitHub
   plugins: [react()],
-  base: '/', // แก้เป็น '/' เพราะตอนนี้ builder.zynksite.com คือหน้าแรกสุดของโปรเจกต์นี้ครับ
 })
