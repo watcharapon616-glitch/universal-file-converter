a80a94f2d7fcd67a0502a89ff9b1fd1a4f8f3867
watcharapon616 <watcharapon616@gmail.com>
2026-01-16 16:47:42 +0700

    Fix MIME type error by setting explicit base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // แก้ไขจุดนี้ครับ
+  base: '/universal-file-converter/', // เปลี่ยนตรงนี้ให้เป็นชื่อ repository
   plugins: [react()],
 })
