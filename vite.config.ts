96d8b4688e57bfd964c8fd4ce6b318c5f6b833f9
watcharapon616 <watcharapon616@gmail.com>
2026-01-16 16:44:48 +0700

    Fix white screen by setting base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ใช้ './' เพื่อให้รองรับทั้งโดเมนส่วนตัวและลิงก์ GitHub
+  base: './', // แก้ไขจุดนี้ครับ
   plugins: [react()],
 })
