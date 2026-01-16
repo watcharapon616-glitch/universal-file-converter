5bf35227f5b4ba68000b8684df348544c4124a7c
watcharapon616 <watcharapon616@gmail.com>
2026-01-16 16:57:42 +0700

    Restore standard paths and set base to relative

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/universal-file-converter/', // เปลี่ยนตรงนี้ให้เป็นชื่อ repository
   plugins: [react()],
+  base: './', // สำคัญมาก: ใช้จุดและสแลช เพื่อให้รองรับการรันบน GitHub Pages
 })
