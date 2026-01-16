60ed86150358da2bac67f02dc5646d847fe9cf75
watcharapon616 <watcharapon616@gmail.com>
2026-01-16 16:59:59 +0700

    Final fix for path and white screen

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // สำคัญมาก: ใช้จุดและสแลช เพื่อให้รองรับการรันบน GitHub Pages
+  base: '/universal-file-converter/', // ต้องมี / ปิดหัวท้ายแบบนี้เป๊ะๆ ครับ
 })
