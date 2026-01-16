7e05d3284d3678c560c775ad0561644330f29354
watcharapon616-glitch <watcharapon616@gmail.com>
2026-01-16 10:44:00 +0000

    Deploying to gh-pages from @ watcharapon616-glitch/universal-file-converter@aacd8bd26d8683f05ad6df5378c39e0dfdc99076 🚀

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {}, // ตรวจสอบว่ามีตัว @ และ /postcss ครบถ้วน
-    autoprefixer: {},
-  },
-}
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // เปลี่ยนจาก '/universal-file-converter/' เป็นแค่ '/' ตัวเดียว
-})
